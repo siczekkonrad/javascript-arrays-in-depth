9a0f24a5f3a5297e5a33d57a0655b6206dc906d9
siczekkonrad <konradd.siczek@gmail.com>
2017-01-05 19:59:58 +0100

    filter array with truthy values

---
+++ 6_filter_array_with_truthy_values.js
@@ -0,0 +1,36 @@
+const lessons = [
+    {
+        title: 'Javascript Arrays in Depth - join',
+        views: 960,
+        tags: ['array', 'join']
+    },
+    {
+        title: 'Javascript Arrays in Depth - concat',
+        views: 1050,
+        tags: ['array', 'concat']
+    },
+    {
+        title: 'Javascript Arrays in Depth - slice',
+        views: 2503,
+        tags: ['array', 'slice']
+    },
+    {
+        title: 'Javascript Functions in Depth - bind',
+        views: 2500,
+        tags: ['functions', 'bind']
+    }
+];
+
+var minViews = 1000;
+var searchTerm = 'array';
+
+var filtered = lessons
+    .filter(x => x.views > minViews)
+    .sort((a, b) => b.views - a.views)
+    .map(x => `     <li>${x.title}</li>`)
+    .join('\n');
+
+
+console.log(`<ul>
+${filtered}
+</ul>`);
